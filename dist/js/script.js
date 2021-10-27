1e159c3e68ba1e88f201fd452aa00b35bc2bf460
Artem <lyubavin.web@gmail.com>
2021-10-27 13:22:54 +0300

    Add pattern for main block while search is active

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const mainTitle = document.querySelector('.search__title'),
+    //=================================================================== first animation
+    const mainLogo = document.querySelector('.header__logo'),
+        mainWrapper = document.querySelector('.search__wrapper'),
+        mainTitle = document.querySelector('.search__title'),
         mainSubtitle = document.querySelector('.search__subtitle'),
         mainInput = document.querySelector('.search__input'),
         mainButton = document.querySelector('.search__button'),
@@ -17,4 +20,15 @@ window.addEventListener('DOMContentLoaded', () => {
     setTimeout(showBlock, 800, mainInput, 'op-1');
     setTimeout(showBlock, 1200, mainButton, 'op-1');
     setTimeout(showBlock, 1200, mainPhoto, 'op-1_img');
+
+    //=================================================================== active btn
+    mainButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        mainWrapper.classList.add('search__wrapper_active');
+    });
+
+    mainLogo.addEventListener('click', (e) => {
+        e.preventDefault();
+        mainWrapper.classList.remove('search__wrapper_active');
+    });
 });
