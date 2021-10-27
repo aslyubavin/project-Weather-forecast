b63b6c79a4070ac9e352fb333162b16977b523f0
Artem <lyubavin.web@gmail.com>
2021-10-27 18:58:29 +0300

    Add weather section

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
         mainWrapper = document.querySelector('.search__wrapper'),
         mainTitle = document.querySelector('.search__title'),
         mainSubtitle = document.querySelector('.search__subtitle'),
+        mainForm = document.querySelector('.search__form'),
         mainInput = document.querySelector('.search__input'),
         mainButton = document.querySelector('.search__button'),
         mainPhoto = document.querySelector('.search__photo');
@@ -22,13 +23,35 @@ window.addEventListener('DOMContentLoaded', () => {
     setTimeout(showBlock, 1200, mainPhoto, 'op-1_img');
 
     //=================================================================== active btn
-    mainButton.addEventListener('click', (e) => {
-        e.preventDefault();
+    mainButton.addEventListener('click', () => {
         mainWrapper.classList.add('search__wrapper_active');
     });
 
-    mainLogo.addEventListener('click', (e) => {
-        e.preventDefault();
+    mainLogo.addEventListener('click', () => {
         mainWrapper.classList.remove('search__wrapper_active');
+    });
+
+    //=================================================================== weather api
+    const api = {
+        key: 'c04385b4f6f09f048e333ea817afc1ca',
+        url: 'https://api.openweathermap.org/data/2.5/'
+    };
+
+    function getData(value) {
+        fetch(`${api.url}weather?q=${value}&appid=${api.key}`)
+            .then(data => {
+                return data.json();
+            })
+            .then(displayData);
+    }
+
+    function displayData(data) {
+        console.log(data);
+    }
+
+    mainForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        getData(mainInput.value);
+        mainForm.reset();
     });
 });
