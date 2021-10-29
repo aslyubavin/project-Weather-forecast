cf0ff1273860b9f6b6291509efd158a293a236dd
Artem <lyubavin.web@gmail.com>
2021-10-29 14:52:37 +0300

    Add display data from API

--- dist/js/script.js
+++ dist/js/script.js
@@ -30,10 +30,29 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     //=================================================================== weather api
+    mainForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        getData(mainInput.value);
+        mainForm.reset();
+        mainWrapper.classList.add('search__wrapper_active');
+        weatherBlock.classList.remove('disN');
+    });
+
     const api = {
         key: 'c04385b4f6f09f048e333ea817afc1ca',
         url: 'https://api.openweathermap.org/data/2.5/'
     };
+
+    let city = document.querySelector('.weather__main-city'),
+        date = document.querySelector('.weather__main-date'),
+        temp = document.querySelector('.weather__main-temp'),
+        icon = document.querySelector('.weather__main-icon'),
+        descr = document.querySelector('.weather__main-descr'),
+        feel = document.querySelector('.weather__main-feel'),
+        minmaxTemp = document.querySelector('[data-article="minmax"]'),
+        humidity = document.querySelector('[data-article="humidity"]'),
+        pressure = document.querySelector('[data-article="pressure"]'),
+        wind = document.querySelector('[data-article="wind"]');
 
     function getData(value) {
         fetch(`${api.url}weather?q=${value}&appid=${api.key}`)
@@ -45,13 +64,38 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function displayData(data) {
         console.log(data);
+        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let currentDate = new Date(data.dt * 1000);
+
+        if (data.cod == 400 || data.cod == 404) {
+            city.innerText = 'City not found';
+            date.innerText = 'Enter the correct city';
+            temp.innerHTML = '--&deg';
+            icon.src = `img/error-icon.svg`;
+            descr.innerText = '';
+            feel.innerHTML = '';
+    
+            minmaxTemp.innerHTML = '--';
+            humidity.innerText = '--';
+            pressure.innerText = '--';
+            wind.innerText = '--';
+        } else {
+            city.innerText = data.name;
+            date.innerText = `${monthNames[currentDate.getMonth()]} ${currentDate.getDate()} ${currentDate.getFullYear()}`;
+            temp.innerHTML = `${Math.round(+data.main.temp - 273.15)}&deg`;
+            icon.src = `img/${data.weather[0].icon}-icon.svg`;
+            descr.innerText = toUpperCaseFirstChar(data.weather[0].description);
+            feel.innerHTML = `Feels like ${Math.round(data.main.feels_like - 273.15)}&deg`;
+    
+            minmaxTemp.innerHTML = `${Math.round(+data.main.temp_min - 273.15)}&deg / ${Math.round(+data.main.temp_max - 273.15)}&deg`;
+            humidity.innerText = `${data.main.humidity} %`;
+            pressure.innerText = `${data.main.pressure} mb`;
+            wind.innerText = `${Math.round(data.wind.speed)} m/s`;
+        }
     }
 
-    mainForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        getData(mainInput.value);
-        mainForm.reset();
-        mainWrapper.classList.add('search__wrapper_active');
-        weatherBlock.classList.remove('disN');
-    });
+    function toUpperCaseFirstChar(str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
 });
